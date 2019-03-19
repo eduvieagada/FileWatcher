a510be397b5f225232cef845576abaa7b5f1ce90
Eduvie Agada <eagada@parkwayprojects.com>
2019-03-19 15:17:20 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\Parkway\Temp\MassPAY\Data\UploadedFiles";
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.FileName | NotifyFilters.Size;
+            watcher.Filter = "*.*";
+
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+            while (Console.Read() != 'q');
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($" {e.OldFullPath} has been renamed to  {e.FullPath}");
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($" {e.Name} has been {e.ChangeType}");
+        }
+    }
+}
